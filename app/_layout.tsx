cf352d58812a9212e25d0eaf1f904a48bb9a3b78
saif <saifahmedraafat@gmail.com>
2026-04-10 19:24:12 -0600

    feat(app): refactor HomeScreen layout and remove unused components chore: clean up project by deleting modal and related assets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout() {
           }}
         />
         <Stack.Screen name="pair-device" options={{ presentation: 'modal', title: 'Pair Device' }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
